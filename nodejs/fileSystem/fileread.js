227871b12596d3690ee91a203f1b79538b9e67a9
ABC <ABC@DESKTOP-S1DF2OB>
2019-12-04 01:54:03 +0900

    update

--- nodejs/fileSystem/fileread.js
+++
@@ -1,7 +0,0 @@
-//https://nodejs.org/dist/latest-v12.x/docs/api/
-var fs = require('fs');
-fs.readFile('sample.txt', 'utf8',function(err, data){
-  console.log(err);
-  console.log(data);
-
-});
--- nodejs/readDir/main.js
+++
@@ -1,6 +0,0 @@
-const folder = '../';
-const fs = require('fs');
-
-fs.readdir(folder, function(err, filelist){
-  console.log(filelist);
-})
